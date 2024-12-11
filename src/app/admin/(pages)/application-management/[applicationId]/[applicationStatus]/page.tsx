0154aa3379cdab0d6aeefc8b913724447f44263d
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2024-12-11 16:22:06 +0800

    fixed eslint

--- src/app/admin/(pages)/application-management/[applicationId]/[applicationStatus]/page.tsx
+++ src/app/admin/(pages)/application-management/[applicationId]/[applicationStatus]/page.tsx
@@ -1,26 +1,26 @@
 import AccountModal from "@/components/modals/account-modal";
 import { Modal } from "@/components/ui/modal";
 import db from "@/lib/db";
+import { GetServerSidePropsContext } from "next";
 import React from "react";
 
 const AccountCreation = async ({
   params,
-}: {
-  params: { applicationId: string };
-}) => {
+}: GetServerSidePropsContext<{ applicationId: string }>) => {
   const data = await db.jobApplication.findFirst({
     where: {
-      id: params.applicationId,
+      id: params?.applicationId,
     },
     include: {
       jobApplicant: true,
     },
   });
+
   return (
     <>
       <Modal
         title={`Create Account For ${data?.jobApplicant.name}`}
-        description="Create an account for the applicant. The system provide the applicant with a temporary password that they can change later."
+        description="Create an account for the applicant. The system provides the applicant with a temporary password that they can change later."
         isOpen
       >
         {data && <AccountModal data={data} />}
