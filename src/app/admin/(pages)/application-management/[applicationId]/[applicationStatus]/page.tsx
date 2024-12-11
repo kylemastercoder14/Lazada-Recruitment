569df51d1078c1e6ab5c92417d208fa33d168e86
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2024-12-11 16:29:15 +0800

    fixed eslint

--- src/app/admin/(pages)/application-management/[applicationId]/[applicationStatus]/page.tsx
+++ src/app/admin/(pages)/application-management/[applicationId]/[applicationStatus]/page.tsx
@@ -1,15 +1,19 @@
 import AccountModal from "@/components/modals/account-modal";
 import { Modal } from "@/components/ui/modal";
 import db from "@/lib/db";
-import { GetServerSidePropsContext } from "next";
 import React from "react";
 
-const AccountCreation = async ({
-  params,
-}: GetServerSidePropsContext<{ applicationId: string, applicationStatus: string }>) => {
+interface AccountCreationProps {
+  params: {
+    applicationId: string;
+    applicationStatus: string;
+  };
+}
+
+const AccountCreation: React.FC<AccountCreationProps> = async ({ params }) => {
   const data = await db.jobApplication.findFirst({
     where: {
-      id: params?.applicationId,
+      id: params.applicationId,
     },
     include: {
       jobApplicant: true,
@@ -17,15 +21,13 @@ const AccountCreation = async ({
   });
 
   return (
-    <>
-      <Modal
-        title={`Create Account For ${data?.jobApplicant.name}`}
-        description="Create an account for the applicant. The system provides the applicant with a temporary password that they can change later."
-        isOpen
-      >
-        {data && <AccountModal data={data} />}
-      </Modal>
-    </>
+    <Modal
+      title={`Create Account For ${data?.jobApplicant.name}`}
+      description="Create an account for the applicant. The system provides the applicant with a temporary password that they can change later."
+      isOpen
+    >
+      {data && <AccountModal data={data} />}
+    </Modal>
   );
 };
 
